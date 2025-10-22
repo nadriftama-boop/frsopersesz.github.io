d989fff4492cae81e0ddc6173dd0c40a79a219fb
nadriftama-boop <nadriftama@gmail.com>
2025-10-22 12:07:59 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function showPage(id) {
+  document.querySelectorAll("section").forEach(sec => {
+    sec.classList.remove("active");
+  });
+  document.getElementById(id).classList.add("active");
+}
